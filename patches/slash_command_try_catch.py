f0168910711affd4ea7bd8041b37c742eb25fe49
NotKenftr <justkenftr@gmail.com>
2026-09-22 21:30:29 +0700

    [Swit v1.1.5] update slash_command patches

--- patches/slash_command_try_catch.py
+++ patches/slash_command_try_catch.py
@@ -5,7 +5,6 @@ _original_do_call = Command._do_call
 
 
 def install_slash_command_try_catch_patch(swit):
-
     async def slash_command_try_catch_patch(
         self,
         interaction: discord.Interaction,
@@ -18,7 +17,22 @@ def install_slash_command_try_catch_patch(swit):
                 params,
             )
         except Exception as e:
-            await swit.get_logger().error(e)
+            logger = swit.get_logger()
+
+            command_name = self.name
+            qualified_name = self.qualified_name
+            module = self.callback.__module__
+
+            await logger.warning(f"-"*50)
+            await logger.warning(
+                "Error occurred while trying to execute slash command: "
+                f"{qualified_name} "
+                f"(name={command_name}, module={module}, "
+                f"user={interaction.user}, guild={interaction.guild})"
+            )
+            await logger.warning(f"-" * 50)
+            await logger.error(e)
+            await logger.warning(f"-" * 50)
             raise
 
     Command._do_call = slash_command_try_catch_patch
