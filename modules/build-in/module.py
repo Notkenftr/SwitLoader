82576bf8d0050b0ad1a817603cbc4626f44c57d8
NotKenftr <justkenftr@gmail.com>
2026-09-23 14:36:26 +0700

    [Swit v1.1.7p] add build-in.unload command

--- modules/build-in/module.py
+++ modules/build-in/module.py
@@ -14,11 +14,34 @@ class BuildIn(commands.Cog):
         self.bot = bot
         self.logger: Logger = bot.get_logger()
     @commands.command(name="unload")
-    async def build_in(self, ctx: commands.Context,module_name: str):
-        print("trigger")
-        await ctx.send(module_name)
+    async def build_in(
+            self,
+            ctx: commands.Context,
+            module_name: str,
+    ):
+        if ctx.author.id not in self.bot.get_swit_config().get("Swit",{}).get("allow_build_in_command",[]):
+            return
 
+        msg = await ctx.send(f"Unloading `{module_name}`...")
+        status, error = await self.loader.unload_module_by_name(
+            module_name,
+            debug=True,
+        )
+        if status:
+            await msg.edit(content=f"Successfully unloaded `{module_name}`.")
+            return
 
+        match error:
+            case "Err: module not loaded":
+                content = f"Module `{module_name}` is not loaded."
+            case "Err: cog not found":
+                content = f"Cog for module `{module_name}` was not found."
+            case _:
+                content = f"Failed to unload module `{module_name}`."
+
+        await msg.edit(content=content)
+
+        
 Manifest = ModuleManifest(
     entry=BuildIn,
     module_type=ModuleType.PREFIX_COMMAND,
